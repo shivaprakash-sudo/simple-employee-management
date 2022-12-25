405e9d9c3524dd3f2453f766c871cb901a302463
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-12-25 07:00:37 +0000

    add exception handling

--- controllers/employees.js
+++ controllers/employees.js
@@ -5,21 +5,37 @@ import LeadEmployee from "../models/LeadEmployee.js";
 // util
 import catchAsync from "../utils/catchAsync.js";
 
-export const addEmployee = catchAsync(async (req, res) => {
+export const addEmployee = (req, res) => {
   const { name, email, leadName } = req.query;
-  const lead = await LeadEmployee.findOne({ name: leadName });
-  const employee = new Employee({
-    name,
-    email,
-    lead: lead._id,
-  });
-  const updatedLead = await LeadEmployee.findByIdAndUpdate(lead._id, {
-    $push: { teamMembers: employee._id },
-  });
-  await employee.save();
-  await updatedLead.save();
-  res.send("Succesfully added employee.");
-});
+  if (!name || !email || !leadName) {
+    res.send("Please enter necessary information");
+  } else {
+    Employee.findOne({ email }).exec(async (err, employee) => {
+      if (employee) {
+        res.send("Wow! Employee already exists.");
+      } else {
+        const lead = await LeadEmployee.findOne({ name: leadName });
+        if (lead) {
+          const employee = new Employee({
+            name,
+            email,
+            lead: lead._id,
+          });
+          const updatedLead = await LeadEmployee.findByIdAndUpdate(lead._id, {
+            $push: { teamMembers: employee._id },
+          });
+          await employee.save();
+          await updatedLead.save();
+          res.send("Succesfully added employee.");
+        } else {
+          res.send(
+            "The lead doesn't seem to be in our database, please check and try again."
+          );
+        }
+      }
+    });
+  }
+};
 
 export const getAllEmployees = catchAsync(async (req, res) => {
   const employees = await Employee.find({});
